04557526ed8258f067d07e81b0da040bce6119de
Stan Chan <stan@Stans-MacBook-Pro.local>
2015-05-04 23:39:21 -0400

    Robin Hood Comments

--- robinHoodHash.h
+++ robinHoodHash.h
@@ -49,7 +49,7 @@ namespace cs375 {
         //returns false if key is already in table
         bool insertKey(Key k){
             std::hash <Key> hash_fn;
-            std::size_t hashValue = hash_fn(k) % tableSize;
+            std::size_t hashIndex = hash_fn(k) % tableSize;
             
             //if load factor is hit then must transfer items to new sparse array
             if(should_resize(0.75f, tableSize, robinArray.size()+1)){
@@ -75,66 +75,82 @@ namespace cs375 {
             //a value that has traveled a greater distance from the hashIndex it wanted to be placed at initially can kick out a value that has traveled a smaller distance even if that value is closer to where it wanted to be initially inserted
             std::size_t currentDistance = 0;
 
-            //while the hash array is full
-            while(robinArray.contains(hashValue)){
-                if (*robinArray.at(hashValue) == k) {
+            //while the hash array location is full
+            while(robinArray.contains(hashIndex)){
+                //if the value already exists we return false
+                if (*robinArray.at(hashIndex) == k) {
                     return false;
                 }
-                if(currentDistance > distanceVector[hashValue]){
-                    std::swap(currentDistance, distanceVector[hashValue]);
-                    std::swap(k,*robinArray.at(hashValue));
+                //if our currentDistance is greater that means we should put our current key and distance that location and hold the key value and distance value that we have just taken the sport of
+                //we keep doing this until we an empty spot for us to insert our key and distance into because that way we won't have to hold a new key and distance
+                if(currentDistance > distanceVector[hashIndex]){
+                    //swap the distances and swap the keys
+                    //find a new home for the distance and keys we got
+                    std::swap(currentDistance, distanceVector[hashIndex]);
+                    std::swap(k,*robinArray.at(hashIndex));
                 }
+                //increment the distance because we are going to check the next index location
                 currentDistance++;
-                hashValue++;
-                if(hashValue == tableSize){
-                    hashValue = 0;
+                //increment the index
+                hashIndex++;
+                //if index is the tableSize we look back around to front of the table
+                if(hashIndex == tableSize){
+                    hashIndex = 0;
                 }
             }
-            robinArray.emplace(hashValue, std::move(k));
-            bitVector[hashValue] = true;
-            distanceVector[hashValue] = currentDistance;
+            //this means we have reached a blank index so we can insert our values here
+            robinArray.emplace(hashIndex, std::move(k));
+            bitVector[hashIndex] = true;
+            distanceVector[hashIndex] = currentDistance;
             return true;
         }
         
+        //search for the key and return index if found
+        //if not return -1
         std::size_t searchKeyIndex(const Key &k){
             std::hash<Key> hash_fn;
-            std::size_t hashValue = hash_fn(k) % tableSize;
+            std::size_t hashIndex = hash_fn(k) % tableSize;
             
             std::size_t currentDistance = 0;
             
-            while(bitVector[hashValue]) {
-                auto ptr = robinArray.at(hashValue);
+            //checks to see if a value is or used to exist there
+            while(bitVector[hashIndex]) {
+                auto ptr = robinArray.at(hashIndex);
+                //if the value isn't equal to k and its not null
                 if(!ptr || *ptr != k){
-                    //1 signifies that location is full or used to be full
-                    if(distanceVector[hashValue] >= currentDistance){
-                        hashValue++;
+                    //if the current value in that hashIndex has a distance greater than or equal to ours than we can't place our value there
+                    if(distanceVector[hashIndex] >= currentDistance){
+                        hashIndex++;
                         currentDistance++;
                     }
                     //signfies this location never had a value
-                    //or that the current distance is greater than distance at this location and should've been inserted here
+                    //or that the current distance is greater than distance at this location and should've been inserted here or earlier so since its not here it means that key doesn't exist
                     else{
                         return -1;
                     }
-                    
-                    if(hashValue == tableSize){
-                        hashValue = 0;
+                    //loops around if we hit end of table
+                    if(hashIndex == tableSize){
+                        hashIndex = 0;
                     }
                 }
                 else{
-                    return hashValue;
+                    return hashIndex;
                 }
             }
             return -1;
         }
         
+        //returns true if key is found
         bool searchKey(const Key &k){
             return searchKeyIndex(k) != (std::size_t) -1;
         }
         
+        //returns true if key is found and erases it
         bool deleteKey(const Key &k){
-            std::size_t hashValue = searchKeyIndex(k);
-            if(hashValue != (std::size_t)-1){
-                robinArray.erase(hashValue);
+            std::size_t hashIndex = searchKeyIndex(k);
+            //if hashIndex is -1 it means the key doesn't exist
+            if(hashIndex != (std::size_t)-1){
+                robinArray.erase(hashIndex);
                 return true;
             }
             return false;
